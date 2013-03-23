3d25066433620dab7aaf286c3a3dee7c887f5085
Charuroopa <cvarada@ncsu.edu>
2013-03-23 16:03:00 -0400

    adding from mac

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minifb::Application.config.secret_token = '045cef40eb4d2a591618ccbbd7f0e638f7fc56920862efd91e495c2a7373961aa1053279f5f6c101fd74300ada4224eb0699bc86746044b78f0efa558d455a8c'
