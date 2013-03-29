7e419a12dc0d98431f9abc574afa01de9f819e9b
Charu <cvarada@ncsu.edu>
2013-03-28 22:25:46 -0400

    Added tests for module user(first name,last name etc)

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	test " a user should enter a first name"  do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+
+		
+	end
+
+	test " a user should enter a Last name"  do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+
+		
+	end
+
+	test " a user should enter a profile name"  do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+
+		
+	end
+
+	test " a user should have unique profile name"  do
+		user = User.new
+		user.profile_name = users(:profil).profile_name
+
+		assert !user.save
+		puts user.errors.inspect
+		assert !user.errors[:profile_name].empty?
+
+		
+	end
+
+	test " a user should have  profile name without spaces"  do
+		user = User.new
+		user.profile_name = "AB DE"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly")
+
+		
+	end
+
+
 end
